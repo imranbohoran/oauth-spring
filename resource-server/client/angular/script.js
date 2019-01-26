2511cb3db94b92f51beb6e3c90483a0817152c7c
Imran Bohoran <imranbohoran@gmail.com>
2019-01-26 11:12:23 +0000

    A working client
     * Calls the auth-server to obtain a auth-code
       and a token for calling the api
     * The code is expected to be in the url which
       would be how a returning resource would receive it
     * If the code is present it's used to make an
       api call
     * Very basic, but proves the point

--- resource-server/client/angular/script.js
+++ resource-server/client/angular/script.js
@@ -1,27 +1,53 @@
-// $scope.uuid = function () {
-//     $http.get('http://localhost:8080/v1/api/uuid')
-//         .success(function(response) {
-//             $scope.results = response;
-//         })
-//         .error(function(error) {
-//             console.log("error is : " + JSON.stringify(error))
-//         })
-// }
-
 
 var testClient = angular.module('testClient', []);
 
 testClient.controller('uuidController' , function ($scope, $http) {
     $scope.home = "This is the UUID generator";
 
+    function extractToken(code) {
+        var params = window.location.search.substr(1).split('&');
+
+        for (var i=0; i<params.length; i++) {
+            var processingParam = params[i].split('=');
+
+            if (processingParam[0] === code) {
+                return processingParam[1];
+            }
+        }
+
+        return undefined;
+    }
+
     $scope.generateUUID = function () {
         console.log("UUID requested!");
-        $http.get("http://localhost:8080/v1/api/uuid", { headers: {'Access-Control-Allow-Origin': '*'}})
-            .then(function successCallback(response){
-                $scope.response = response;
-            }, function errorCallback(response){
-                console.log("Unable to perform get request");
-            });
+
+        var token = extractToken('code');
+        console.log("Working with token: "+ token);
+
+        var config = {
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Authorization': 'Bearer '+ token
+            }
+        };
+
+        $http.get("http://localhost:8091/example-resource-server/v1/api/uuid", config)
+            .then(function (response) {
+                console.log("Successful with data:"+ response.data);
+                $scope.uuidValue = response.data;
+            })
+            .catch(function errorCallback(response) {
+                console.log("Unable to perform get request: " + response);
+                console.log("Currently at : " + window.location);
+                if (response.status === 401) {
+                    var returnUrl = window.location.origin + window.location.pathname;
+                    location.href = 'http://localhost:8090/example-oauth-server/oauth/authorize?client_id=example_client_id&redirect_uri=http://localhost:8091/example-resource-server/v1/auth/authenticated?client_redirect_uri='+ returnUrl + '&response_type=code&grant_type=authorization_code';
+                }
+            })
+            .finally(function () {
+                console.log("Done!!")
+            })
+        ;
     };
 
 });
